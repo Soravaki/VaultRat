bd2f1f6e4ad81c158fdbb4ef2912206343f0e3cc
Soravaki <92048405+Soravaki@users.noreply.github.com>
2022-02-10 13:51:32 -0600

    School Works

--- src/VaultMap.java
+++ src/VaultMap.java
@@ -5,9 +5,6 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class VaultMap extends Graph{
-    String[] inputLine1;
-    String[] inputLine2;
-
     VaultMap(String filename) throws IOException {
         //adjmap = new HashMap<>();
         // Instantiate Scanner and takes in input for how many mazes
@@ -16,29 +13,61 @@ public class VaultMap extends Graph{
         sc.nextLine(); // scanner refresh
 
         while (numMaze != 0) {
-            int num = 1;
+            Graph g = new Graph();
             String[] dimensions = sc.nextLine().split(" ");
             int length = Integer.parseInt(dimensions[0]);
             int height = Integer.parseInt(dimensions[1]);
             System.out.println("dimensions in");
             // ties the characters together into a graph
             // until num = height -1
-            while (num < height){
-                // splits the line and imports the info into a graph
-                    String[] inputLine1 = sc.nextLine().split("|");
-                    String[] inputLine2 = sc.nextLine().split("|");
-                System.out.println(Arrays.toString(inputLine1)); // debug
 
-                for (int i=1; i<length; i++){
-                    // left to right
-                    addEdge(inputLine1[i-1], inputLine1[i]);
-                    // up to down
-                    addEdge(inputLine1[i-1], inputLine2[i-1]);
-                    addEdge(inputLine1[i], inputLine2[i]);
-                }
-                num++;
-                System.out.println(Arrays.toString(inputLine2)); // debug
+            // creates 2d array that stores the vaultmap and another array to act its location
+            String[][] maze = new String[height][length];
+            int[][] adjMazeRef = new int[height][length];
+            // creates maze
+            for (int i=0; i<height; i++){
+                maze[i] = sc.nextLine().split("|");
             }
+            // creates maze reference for making adjacent list
+            int num=0;
+            for (int y=0; y<height; y++){
+                for (int x=0; x<length; x++){
+                    adjMazeRef[y][x] = num;
+                    num++;
+                }
+            }
+
+            // traverses 2d array to make adjList
+            for (int y=0; y<height; y++){
+                for (int x=0; x<length; x++){
+                    System.out.print(adjMazeRef[y][x] + " ");
+                    // addEdge to the right if in bounds
+                    if (x<length-1 && !(hasVertex( Integer.toString(adjMazeRef[y][x]))))
+                        g.addEdge(Integer.toString(adjMazeRef[y][x]), Integer.toString((adjMazeRef[y][x]+1)));
+
+                    // addEdge to the left if in bounds
+                    if (x>0 && !(hasVertex( Integer.toString(adjMazeRef[y][x]))))
+                        g.addEdge(Integer.toString(adjMazeRef[y][x]), Integer.toString(( adjMazeRef[y][x]-1)));
+
+                    // addEdge above if in bounds
+                    if (y>0 && !(hasVertex( Integer.toString(adjMazeRef[y][x]))))
+                        g.addEdge(Integer.toString(adjMazeRef[y][x]), Integer.toString((adjMazeRef[y][x]-height)));
+
+                    // addEdge below if in bounds
+                    if (y<height-1 && !(hasVertex( Integer.toString(adjMazeRef[y][x]))))
+                        g.addEdge(Integer.toString(adjMazeRef[y][x]), Integer.toString((adjMazeRef[y][x]+height)));
+                }
+            }
+
+            System.out.println(g.adjmap.keySet());
+            /*for (int i=0; i<length*height; i++){
+                System.out.println(adjmap.containsKey(Integer.toString(i)));
+            }*/
+
+            /*// debug - prints out maze
+            for (int i=0; i<height; i++){
+                System.out.println(Arrays.toString(adjMazeRef[i]));
+            }*/
 
             System.out.println("compiled. length : " + length + " height : " + height);
         }
